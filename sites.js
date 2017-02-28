fe34c92c48bab3a0829788f7f99cb41fa51fc7b6
Christopher Van Wiemeersch <hearcomestreble@gmail.com>
2017-02-28 14:41:02 -0500

    add local-dev URL rewrites for WebVR Agent and site start URLs + favicons; add `<meta>` tags for `vrDefaultDisplay` and `vrAvailableDisplays` (to reflect values in manifest)

--- sites.js
+++ sites.js
@@ -1,9 +1,31 @@
+var IS_DEV = window.location.port !== 80 && window.location.port !== 443;
+var ORIGIN = window.location.protocol + '//' + window.location.host;
+var ORIGIN_NO_PORT = window.location.protocol + '//' + window.location.hostname;
+
+if (IS_DEV) {
+  var webvrAgentScript = document.querySelector('script[src*="agent"][src*="/client.js"]');
+  if (webvrAgentScript) {
+    webvrAgentScript.src = webvrAgentScript.src.replace(/(.+)client.js/, ORIGIN_NO_PORT + ':4040/client.js');
+  }
+}
+
+var puzzle_rain = {
+  name: 'Puzzle Rain',
+  url: 'https://mozvr.com/puzzle-rain/?mode=normal&src=moonrise',
+  favicon: 'https://mozvr.com/puzzle-rain/models/gltf/puzzle-rain.gltf'
+};
+
+if (IS_DEV) {
+  ['url', 'favicon'].forEach(function (key) {
+    puzzle_rain[key] = puzzle_rain[key].replace(
+      'https://mozvr.com/puzzle-rain/',
+      ORIGIN_NO_PORT + ':9966/'
+    );
+  });
+}
+
 module.exports = [
-  {
-    name: 'Puzzle Rain',
-    url: 'https://mozvr.com/puzzle-rain/',
-    favicon: 'models/puzzle-rain/puzzle-rain.gltf'
-  },
+  puzzle_rain,
   {
     name: 'A-Painter',
     url: 'https://aframe.io/a-painter/'
