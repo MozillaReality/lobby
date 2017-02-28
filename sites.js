b38a754378c6e1be736be54251d7079ee74cb647
Christopher Van Wiemeersch <hearcomestreble@gmail.com>
2017-02-28 04:47:16 -0500

    fix link to A-Blast

--- sites.js
+++ sites.js
@@ -1,23 +1,23 @@
 module.exports = [
-{
-  name: 'Puzzle Rain',
-  url: 'https://mozvr.com/puzzle-rain/',
-  favicon: 'models/puzzle-rain/puzzle-rain.gltf'
-},
-{
-  name: 'A-Painter',
-  url: 'https://aframe.io/a-painter'
-},
-{
-  name: 'A-Shooter',
-  url: 'https://aframe.io/a-shooter'
-},
-{
-  name: 'Sketchfab',
-  url: 'https://sketchfab.com/vr'
-},
-{
-  name: 'Finding Love',
-  url: 'https://findinglove.activetheory.net/'
-}
+  {
+    name: 'Puzzle Rain',
+    url: 'https://mozvr.com/puzzle-rain/',
+    favicon: 'models/puzzle-rain/puzzle-rain.gltf'
+  },
+  {
+    name: 'A-Painter',
+    url: 'https://aframe.io/a-painter/'
+  },
+  {
+    name: 'A-Blast',
+    url: 'https://aframe.io/a-blast/'
+  },
+  {
+    name: 'Sketchfab',
+    url: 'https://sketchfab.com/vr'
+  },
+  {
+    name: 'Finding Love',
+    url: 'https://findinglove.activetheory.net/'
+  }
 ];
